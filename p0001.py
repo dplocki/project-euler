4ba60cd76b8b5da1655fe83e2017e39a957137de
Dawid Plocki <dawid.plocki@gmail.com>
2022-04-25 21:36:54 +0200

    Add solution to problem #1

--- p0001.py
+++ p0001.py
@@ -1,1 +1,13 @@
-print(sum(n for n in range(1000) if n % 3 == 0 or n % 5 == 0))
+def simple_solution(upper_limit: int) -> int:
+    return sum(n for n in range(upper_limit) if n % 3 == 0 or n % 5 == 0)
+
+def better_solution(upper_limit: int) -> int:
+
+    def sum_of_divisible_by(divisible: int):
+        last_divisible = (upper_limit - 1) // divisible
+        return divisible * last_divisible * (last_divisible + 1) // 2
+
+    return sum_of_divisible_by(3) + sum_of_divisible_by(5) - sum_of_divisible_by(15)
+
+LIMIT = 1000
+assert simple_solution(LIMIT) == better_solution(LIMIT)
