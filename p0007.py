6c0a436864836279ab921b3397e2552e6edc9b67
Dawid Plocki <dawid.plocki@gmail.com>
2022-04-26 17:36:38 +0200

    Add solution to problem #7

--- p0007.py
+++ p0007.py
@@ -1,18 +1,42 @@
 # https://projecteuler.net/problem=7
 
+def is_prime(n: int) -> bool:
+    if n < 4:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    if n < 9:
+        return True
+
+    if n % 3 == 0:
+        return False
+
+    r = int(n ** 0.5)
+    f = 5
+
+    while f <= r:
+        if n % f == 0:
+            return False
+
+        if n % (f+2) == 0:
+            return False
+
+        f += 6
+
+    return True
+
 def nth_prime(n: int) -> int:
-    primes = { 2 }
-    number = 3
+    count = 1
+    candidate = 1
+    while count < n:
+        candidate = candidate + 2
 
-    while True:
-        if all(number % prime != 0 for prime in primes):
-            if len(primes) == n - 1:
-                return number
+        if is_prime(candidate):
+            count = count + 1
 
-            primes.add(number)
-        
-        number += 2
-
+    return candidate
 
 assert nth_prime(6) == 13
 print(nth_prime(10001))
