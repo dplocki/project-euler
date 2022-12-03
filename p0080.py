b18ec7b2f074cb5b2db1db457358747fba1e9c9d
Dawid Plocki <dawid.plocki@gmail.com>
2022-12-03 18:11:57 +0100

    Change approach in #80

--- p0080.py
+++ p0080.py
@@ -1,16 +1,29 @@
 # https://projecteuler.net/problem=80
-from decimal import getcontext, Decimal
 from math import sqrt
+
+PRECISION = 100
 
 def str_to_digit_sum(n) -> int:
     return sum(map(int, n))
 
-def solution():
-    getcontext().prec = 102
+def square_root(n, digits):
+    limit = 10**(digits + 1)
+    a = 5*n
+    b = 5
+    while b < limit:
+        if a >= b:
+            a -= b
+            b += 10
+        else:
+            a *= 100
+            b = (b//10) * 100 + 5
 
+    return b // 100
+
+def solution():
     return sum(
-        str_to_digit_sum(str(Decimal(number + 1).sqrt()).replace('.', '')[:100])
-        for number in range(100)
+        str_to_digit_sum(str(square_root(number + 1, PRECISION)))
+        for number in range(PRECISION)
         if not sqrt(number + 1) % 1 == 0
     )
 
